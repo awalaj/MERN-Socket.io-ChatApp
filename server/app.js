19c05f925af84e3894160d0b8ada40efe5c0a806
awalaj <94739236+awalaj@users.noreply.github.com>
2022-11-02 19:31:19 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -23,9 +23,6 @@ const socketServer = new socket()
 
 socketServer.socketEvents()
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
