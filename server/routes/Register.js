1a8ad8fd8dead597f0aa3eea7723d605b77df1c9
awalaj <94739236+awalaj@users.noreply.github.com>
2022-11-05 12:47:18 +0700

    Update Register.js

--- server/routes/Register.js
+++ server/routes/Register.js
@@ -6,17 +6,30 @@ const bcryptjs = require("bcryptjs");
 router.post("/", async function(req, res){
     try{
         let data = req.body.data;
-        let secretPassword = await bcryptjs.hash(data.passwordRegister, 10);
+        let password = data.passwordRegister
         
-        await PersonSchema.create({
+        const validateSymbol = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password)
+        const validateLength = password.length >= 8
+        const validateNumber = /[0-9]/.test(password)
+        
+        if(validateSymbol && validateLength && validateNumber){
+           let secretPassword = await bcryptjs.hash(password, 10);
+           await PersonSchema.create({
             username: data.usernameRegister,
             email: data.emailRegister,
             password: secretPassword,
-        })
+           })
+           return res.json({status: 'succes'})
+        }
+        
+        if(!validateSymbol) throw "password must contain the symbol"
+        if(!validateLength) throw "password length must be more than 8 and less than 16"
+        if(!validateNumber) throw "password must contain number"
+        
+        if(!validateNumber && !validateLength && !validateNumber) throw "password must be more than 8 and less than 16, have a number and character"
 
-        return res.json({status: 'succes'})
     }catch(err){
-        return res.json({status: 'error'})
+        return res.json({status: 'error', message: err})
     }
 })
 
